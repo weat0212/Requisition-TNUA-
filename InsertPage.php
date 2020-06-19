fd142ae006f96eda799b36cb1805df8b4d18bb38
weat0212 <weat0212@gmail.com>
2020-06-19 15:33:04 +0800

    new branch:change booking time to list

--- InsertPage.php
+++ InsertPage.php
@@ -1,16 +1,4 @@
 <?php
-	$serverName = "DESKTOP-PGTDFJ7";
-	$connectionInfo = array( "Database"=>"TEST", "UID"=>"andywang", "PWD"=>"andy0212", "CharacterSet" => "UTF-8");
-	$conn=sqlsrv_connect($serverName,$connectionInfo);
-	$applyDate=$_POST['applyDate'];
-	$name=$_POST['contact'];
-	$phone=$_POST['phone'];
-	$mail=$_POST['mail'];
-	$sql="INSERT INTO dbo.comment(applyDate,name,phone,mail) VALUES('$applyDate','$name','$phone','$mail')";
-	$quer=sqlsrv_query($conn, $sql) or die("sql error".sqlsrv_errors());
-?>
-
-<?php
     header("Content-Type:text/html; charset=utf-8");
 ?>
 
@@ -49,7 +37,7 @@
             }else{echo"";}
             
 		}
-
+        //ToDo:應先檢查所有資料已輸入
 		else{
             if(empty($_POST['applyDate'])||empty($_POST['applicant'])||empty($_POST['contact'])||empty($_POST['DeptSupv'])){
                 echo "資料未輸入尚未齊全，請重新登錄資料!";
@@ -64,58 +52,55 @@
             }
 ?>
 
-//ToDo:排演日及演出日AB僅能出現一次
+<!-- ToDo:排演日及演出日AB僅能出現一次 -->
 <?php
-    $A = 0;
-    $B = 0;
-    if(isset($_POST['dat1'])){
-        //check if A/B only input once
-        for($i=1 ; $i<9 ; $i++) {
-            if($_POST['dat1assist'.(string)$i] == 'A' || $_POST['dat1assist'.(string)$i] == 'a'){
-                $A += 1;
-            }else{
-                if($_POST['dat1assist'.(string)$i] == 'B' || $_POST['dat1assist'.(string)$i] == 'b'){
+    // 檢查是否重複輸入
+    function chk_timetable($dat){
+        $A = 0;
+        $B = 0;
+        if(isset($_POST[$dat])){
+            //check if A/B only input once
+            for($i=1 ; $i<9 ; $i++) {
+                if($_POST[$dat.'assist'.(string)$i] == 'A' || $_POST[$dat.'assist'.(string)$i] == 'a'){
+                    $A += 1;
+                }
+                if($_POST[$dat.'assist'.(string)$i] == 'B' || $_POST[$dat.'assist'.(string)$i] == 'b'){
                     $B += 1;
                 }
             }
-        }
-        if($A==1 || $B==1){
-            exit();
+            
+            if($A==1 and $B==1){
+                // ToDo:顯示預約時段
+                echo"您的租用裝台排練時段:"."租用演出時段:</br>";
+            }else{
+                //缺值
+                echo"<font size=3 color=red>租借表單填寫缺值或多輸入值</br>";
+            }
         }else{
-            //缺值
-            echo"租借表單填寫缺值或多輸入值";
+            if($dat=='dat1'){
+                echo"<font size=3 color=red>必須至少輸入一筆租用裝台排練時段、租用演出時段</br>";
+            }
         }
-    }else{
-        echo"未填寫租用裝台排練時段（請填Ａ）、租用演出時段（請填Ｂ）";
     }
 
-    $A = 0;
-    $B = 0;
-    if(isset($_POST['dat2'])){
-
+    for($i=0;$i<3;$i++){
+        chk_timetable('dat'.(string)$i);
     }
-
-    $A = 0;
-    $B = 0;
-    if(isset($_POST['dat3'])){
-
-    }
+    
 ?>
 
-<!-- //ToDo:檢查正則式
-<?php
-    // $retaldatA = 
+<!-- ToDo:檢查正則式 -->
 
-?> -->
 
-        //表格輸入皆正確的情況
+         <!-- 表格輸入皆正確的情況 -->
         <div class="detail_box clearfix">
 			<p class="photo">
                 <img src="images/thanKU.webp" width="250" height="200" alt="謝謝你">
             </p>
             <p class="text">
             <?php
-			if(isset($_POST['contact'])){
+            
+            if(isset($_POST['contact'])){
 				echo '<font size=4>以下是您的基本資料，請確認是否有錯誤：</font><br/><br/><br/>';
 				echo '<li>申請聯絡人：';
 				echo '<font color=#66B3FF>'.$_POST['contact'].'</font></li>';
@@ -125,6 +110,14 @@
 				echo "申請聯絡人資料錯誤</br>";
 			}
 
+            if(isset($_POST['applyDate'])){
+				echo '<li>申請日期：';
+				echo '<font color=#66B3FF>'.$_POST['applyDate'].'</font></li>';
+				echo '</br>';
+			}
+			else{
+				echo "電話號碼錯誤";
+			}
 			if(isset($_POST['mail'])){
 				echo '<li>e-mail：';
 				echo '<font color=#66B3FF>'.$_POST['mail'].'</font></li>';
@@ -148,4 +141,17 @@
 	    </p>
         </div>
 </div>
+
+<!-- 連線認證以及SQL INSERT -->
+<?php
+	$serverName = "DESKTOP-PGTDFJ7";
+	$connectionInfo = array( "Database"=>"FinalProject", "UID"=>"andywang", "PWD"=>"andy0212", "CharacterSet" => "UTF-8");
+	$conn=sqlsrv_connect($serverName,$connectionInfo);
+    $applyDate=$_POST['applyDate'];
+	$name=$_POST['contact'];
+    $phone=$_POST['phone'];
+	$mail=$_POST['mail'];
+	$sql="INSERT INTO dbo.comment(applyDate,name,phone,email) VALUES('$applyDate','$name','$phone','$mail')";
+	$quer=sqlsrv_query($conn, $sql) or die("sql error".sqlsrv_errors());
+?>
 </body></html>
